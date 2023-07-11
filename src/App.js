48c5e75b8639736e59a4ee93a04834298f4569f1
oeanb12 <oeanb12@gmail.com>
2023-07-11 16:07:07 +0900

    data object

--- src/App.js
+++ src/App.js
@@ -2,15 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 function App() {
-  const [list, setList]= useState();
+  const [data, setData]= useState([
+    {
+      idx: "1",
+      os: "ios", 
+      ver: "1.0", 
+      updateType: "true", 
+      message: "null", 
+      package: "com.test.myapp", 
+      regdate: "2023/05/05 12:00:00"
+    },
+    {
+      idx: "2",
+      os: "ios", 
+      ver: "1.0", 
+      updateType: "true", 
+      message: "null", 
+      package: "com.test.myapp", 
+      regdate: "2023/05/05 12:00:00"
+    }, 
+    {
+      idx: "3",
+      os: "android", 
+      ver: "1.5", 
+      updateType: "false", 
+      message: "서비스 A가 업데이트 되었습니다.", 
+      package: "com.test.myapp", 
+      regdate: "2023/05/05 12:00:00"
+    }, 
+    {
+      idx: "4",
+      os: "android", 
+      ver: "1.5", 
+      updateType: "false", 
+      message: "서비스 A가 업데이트 되었습니다.", 
+      package: "com.test.myapp", 
+      regdate: "2023/05/05 12:00:00"
+    }, 
+    {
+      idx: "5",
+      os: "ios", 
+      ver: "2.0", 
+      updateType: "true", 
+      message: "신규서비스 출시 앱을 업데이트...", 
+      package: "com.test.myapp", 
+      regdate: "2023/05/05 12:00:00"
+    }
+  ]);
 
-  function versionList(list) {
+  function versionList(data) {
     let arr = [];
-    for(let i = 1; i <= 5; i++) {
+
+    for(let i = 0; i < data.length; i++) {
       arr.push(
         <tr className="list">
-          <td>{i}</td><td>ios</td><td>1.0</td><td>true</td><td>null</td>
-          <td>com.test.myapp</td><td>2023/05/05 12:00:00</td>
+          <td>{data[i].idx}</td><td>{data[i].os}</td><td>{data[i].ver}</td><td>{data[i].updateType}</td>
+          <td>{data[i].message}</td><td>{data[i].package}</td><td>{data[i].regdate}</td>
           <td>
             <button className="actionBtn" id="testBtn">Test</button>
             <button className="actionBtn" id="updateBtn">수정</button>
@@ -34,9 +81,8 @@ function App() {
           <th>idx</th><th>os</th><th>ver</th><th>updatetype</th>
           <th>message</th><th>package</th><th>regdate</th><th>action</th>
         </tr>
-        {versionList(list)}
+        {versionList(data)}
       </table>
-
     </div>
   );
 }
