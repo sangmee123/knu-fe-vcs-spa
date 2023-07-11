40b09bcc42bcbc90cc12abd7d72dbf680662a43f
oeanb12 <oeanb12@gmail.com>
2023-07-11 15:01:49 +0900

    useState -> list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [list, setList]= useState();
+
+  function versionList(list) {
+    let arr = [];
+    for(let i = 1; i <= 5; i++) {
+      arr.push(
+        <tr className="list">
+          <td>{i}</td><td>ios</td><td>1.0</td><td>true</td><td>null</td>
+          <td>com.test.myapp</td><td>2023/05/05 12:00:00</td>
+          <td>
+            <button className="actionBtn" id="testBtn">Test</button>
+            <button className="actionBtn" id="updateBtn">수정</button>
+            <button className="actionBtn" id="deleteBtn">삭제</button>
+          </td>
+        </tr>
+      )
+    }
+    return arr;
+  }
+
   return (
     <div className="App">
       <div className="mainBtn">
@@ -14,53 +34,9 @@ function App() {
           <th>idx</th><th>os</th><th>ver</th><th>updatetype</th>
           <th>message</th><th>package</th><th>regdate</th><th>action</th>
         </tr>
-
-        <tr className="list">
-          <td>1</td><td>ios</td><td>1.0</td><td>true</td><td>null</td>
-          <td>com.test.myapp</td><td>2023/05/05 12:00:00</td>
-          <td>
-            <button className="actionBtn" id="testBtn">Test</button>
-            <button className="actionBtn" id="updateBtn">수정</button>
-            <button className="actionBtn" id="deleteBtn">삭제</button>
-          </td>
-        </tr>
-        <tr>
-        <td>2</td><td>ios</td><td>1.0</td><td>true</td><td>null</td>
-          <td>com.test.myapp</td><td>2023/05/05 12:00:00</td>
-          <td>
-            <button className="actionBtn" id="testBtn">Test</button>
-            <button className="actionBtn" id="updateBtn">수정</button>
-            <button className="actionBtn" id="deleteBtn">삭제</button>
-          </td>
-        </tr>
-        <tr>
-        <td>3</td><td>ios</td><td>1.0</td><td>true</td><td>null</td>
-          <td>com.test.myapp</td><td>2023/05/05 12:00:00</td>
-          <td>
-            <button className="actionBtn" id="testBtn">Test</button>
-            <button className="actionBtn" id="updateBtn">수정</button>
-            <button className="actionBtn" id="deleteBtn">삭제</button>
-          </td>
-        </tr>
-        <tr>
-        <td>4</td><td>ios</td><td>1.0</td><td>true</td><td>null</td>
-          <td>com.test.myapp</td><td>2023/05/05 12:00:00</td>
-          <td>
-            <button className="actionBtn" id="testBtn">Test</button>
-            <button className="actionBtn" id="updateBtn">수정</button>
-            <button className="actionBtn" id="deleteBtn">삭제</button>
-          </td>
-        </tr>
-        <tr>
-        <td>5</td><td>ios</td><td>1.0</td><td>true</td><td>null</td>
-          <td>com.test.myapp</td><td>2023/05/05 12:00:00</td>
-          <td>
-            <button className="actionBtn" id="testBtn">Test</button>
-            <button className="actionBtn" id="updateBtn">수정</button>
-            <button className="actionBtn" id="deleteBtn">삭제</button>
-          </td>
-        </tr>
+        {versionList(list)}
       </table>
+
     </div>
   );
 }
