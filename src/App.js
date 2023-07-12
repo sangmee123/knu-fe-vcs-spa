3ddff1fcce3cce078ddb9f91d9b2e1c0fe9421b0
oeanb12 <oeanb12@gmail.com>
2023-07-12 11:24:45 +0900

    Asynchronous

--- src/App.js
+++ src/App.js
@@ -1,73 +1,109 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
+
 function App() {
-  const [data, setData]= useState([
-    {
-      idx: "1",
-      os: "ios", 
-      ver: "1.0", 
-      updateType: "true", 
-      message: "null", 
-      package: "com.test.myapp", 
-      regdate: "2023/05/05 12:00:00"
-    },
-    {
-      idx: "2",
-      os: "ios", 
-      ver: "1.0", 
-      updateType: "true", 
-      message: "null", 
-      package: "com.test.myapp", 
-      regdate: "2023/05/05 12:00:00"
-    }, 
-    {
-      idx: "3",
-      os: "android", 
-      ver: "1.5", 
-      updateType: "false", 
-      message: "서비스 A가 업데이트 되었습니다.", 
-      package: "com.test.myapp", 
-      regdate: "2023/05/05 12:00:00"
-    }, 
-    {
-      idx: "4",
-      os: "android", 
-      ver: "1.5", 
-      updateType: "false", 
-      message: "서비스 A가 업데이트 되었습니다.", 
-      package: "com.test.myapp", 
-      regdate: "2023/05/05 12:00:00"
-    }, 
-    {
-      idx: "5",
-      os: "ios", 
-      ver: "2.0", 
-      updateType: "true", 
-      message: "신규서비스 출시 앱을 업데이트...", 
-      package: "com.test.myapp", 
-      regdate: "2023/05/05 12:00:00"
-    }
-  ]);
+  // let list = [
+  //   {
+  //     idx: "1",
+  //     os: "ios", 
+  //     ver: "1.0", 
+  //     updateType: "true", 
+  //     message: "null", 
+  //     packageInfo: "com.test.myapp", 
+  //     regdate: "2023/05/05 12:00:00"
+  //   },
+  //   {
+  //     idx: "2",
+  //     os: "ios", 
+  //     ver: "1.0", 
+  //     updateType: "true", 
+  //     message: "null", 
+  //     packageInfo: "com.test.myapp", 
+  //     regdate: "2023/05/05 12:00:00"
+  //   }, 
+  //   {
+  //     idx: "3",
+  //     os: "android", 
+  //     ver: "1.5", 
+  //     updateType: "false", 
+  //     message: "서비스 A가 업데이트 되었습니다.", 
+  //     packageInfo: "com.test.myapp", 
+  //     regdate: "2023/05/05 12:00:00"
+  //   }, 
+  //   {
+  //     idx: "4",
+  //     os: "android", 
+  //     ver: "1.5", 
+  //     updateType: "false", 
+  //     message: "서비스 A가 업데이트 되었습니다.", 
+  //     packageInfo: "com.test.myapp", 
+  //     regdate: "2023/05/05 12:00:00"
+  //   }, 
+  //   {
+  //     idx: "5",
+  //     os: "ios", 
+  //     ver: "2.0", 
+  //     updateType: "true", 
+  //     message: "신규서비스 출시 앱을 업데이트...", 
+  //     packageInfo: "com.test.myapp", 
+  //     regdate: "2023/05/05 12:00:00"
+  //   }
+  // ];
+  // const [data, setData]= useState(list);
 
-  function versionList(data) {
-    let arr = [];
-
-    for(let i = 0; i < data.length; i++) {
-      arr.push(
-        <tr className="list">
-          <td>{data[i].idx}</td><td>{data[i].os}</td><td>{data[i].ver}</td><td>{data[i].updateType}</td>
-          <td>{data[i].message}</td><td>{data[i].package}</td><td>{data[i].regdate}</td>
-          <td>
-            <button className="actionBtn" id="testBtn">Test</button>
-            <button className="actionBtn" id="updateBtn">수정</button>
-            <button className="actionBtn" id="deleteBtn">삭제</button>
-          </td>
-        </tr>
-      )
-    }
-    return arr;
+  // ajax, jquery 형태의 서버 통신 
+  let arr = [];
+  function getConfigData() {
+    axios
+    .get('http://localhost:8080/vercontrol/getConfigAll')
+    .then(function (resonse) {
+      // 성공한 경우 실행
+      let data = resonse;
+      console.log(data);
+      for(let i = 0; i < data.length; i++) {
+        arr.push(
+          <tr className="list">
+            <td>{data[i].id}</td><td>{data[i].os}</td><td>{data[i].ver}</td><td>{data[i].updateType}</td>
+            <td>{data[i].message}</td><td>{data[i].packageInfo}</td><td>{data[i].regdate}</td>
+            <td>
+              <button className="actionBtn" id="testBtn">Test</button>
+              <button className="actionBtn" id="updateBtn">수정</button>
+              <button className="actionBtn" id="deleteBtn">삭제</button>
+            </td>
+          </tr>
+        )
+      }
+      return arr;
+    })
+    .catch(function (error) {
+      // 에러인 경우 실행
+      console.log(error);
+    })
+    .then(function () {
+      // 항상 실행
+    });
   }
+
+  // function versionList(data) {
+  //   let arr = [];
+
+  //   for(let i = 0; i < data.length; i++) {
+  //     arr.push(
+  //       <tr className="list">
+  //         <td>{data[i].idx}</td><td>{data[i].os}</td><td>{data[i].ver}</td><td>{data[i].updateType}</td>
+  //         <td>{data[i].message}</td><td>{data[i].package}</td><td>{data[i].regdate}</td>
+  //         <td>
+  //           <button className="actionBtn" id="testBtn">Test</button>
+  //           <button className="actionBtn" id="updateBtn">수정</button>
+  //           <button className="actionBtn" id="deleteBtn">삭제</button>
+  //         </td>
+  //       </tr>
+  //     )
+  //   }
+  //   return arr;
+  // }
 
   return (
     <div className="App">
@@ -81,7 +117,8 @@ function App() {
           <th>idx</th><th>os</th><th>ver</th><th>updatetype</th>
           <th>message</th><th>package</th><th>regdate</th><th>action</th>
         </tr>
-        {versionList(data)}
+        {/* {versionList(data)} */}
+        {getConfigData()}
       </table>
     </div>
   );
